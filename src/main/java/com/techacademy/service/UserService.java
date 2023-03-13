6ea449358dc98ad36c0e3507b4db23e638ccf818
satomizawazawa <viva3sx@gmail.com>
2023-03-13 19:54:43 +0900

    User登録機能を追加

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,7 +1,10 @@
 package com.techacademy.service;
 
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
 
@@ -18,4 +21,12 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    // 追加：ここからーーーーー
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // 追加：ここまでーーーーー
 }
